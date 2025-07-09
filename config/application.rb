12de492b80d7a9e219f6a6e42cbfc6bc2c243ac7
Kaile Borbon <airkaile@gmail.com>
2025-07-10 07:58:09 +0800

    set PH timezone for consistent date/time related testing

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,9 @@ module JournalApp
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.2
 
+    config.time_zone = "Asia/Manila"
+    config.active_record.default_timezone = :utc
+
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
